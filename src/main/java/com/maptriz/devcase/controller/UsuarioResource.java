1f3f174569ea80518e8beb3ab5d13bfb95bb8421
giovanacgois <giovanagois@alunos.utfpr.edu.br>
2019-10-31 02:59:50 -0200

    Adicionado método deletarUsuario()

--- src/main/java/com/maptriz/devcase/controller/UsuarioResource.java
+++ src/main/java/com/maptriz/devcase/controller/UsuarioResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,32 +25,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api")
 public class UsuarioResource {
-   
+
     @Autowired
     UsuarioRepository usuarioRepository;
-    
+
     /*
     * GET 
-    */
-    
+     */
     @GetMapping("/usuarios")
-    public List<Usuario> listarUsuarios(){
-    return usuarioRepository.findAll();
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepository.findAll();
     }
-    
+
     @GetMapping("/usuario/{id}")
-    public Usuario listarUsuarioUnico(@PathVariable(value="id")long id){
-    return usuarioRepository.findById(id);
+    public Usuario listarUsuarioUnico(@PathVariable(value = "id") long id) {
+        return usuarioRepository.findById(id);
     }
 
     /*
     * POST  
-    */
-    
+     */
     @PostMapping("/usuario")
-    public Usuario salvarUsuario(@RequestBody Usuario usuario){
-    return usuarioRepository.save(usuario);
+    public Usuario salvarUsuario(@RequestBody Usuario usuario) {
+        return usuarioRepository.save(usuario);
     }
 
+    /*
+    * DELETE  
+     */
+    @DeleteMapping("/usuario")
+    public void deletarUsuario(@RequestBody Usuario usuario) {
+        usuarioRepository.delete(usuario);
+    }
 
+    
 }
