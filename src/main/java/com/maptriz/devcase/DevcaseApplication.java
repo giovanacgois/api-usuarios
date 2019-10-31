5c9af08931a3a3f927101c80154c9d36fec4813b
giovanacgois <giovanagois@alunos.utfpr.edu.br>
2019-10-31 00:20:19 -0200

    Inicialização do Projeot

---
+++ src/main/java/com/maptriz/devcase/DevcaseApplication.java
@@ -0,0 +1,13 @@
+package com.maptriz.devcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maptriz/devcase/DevcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maptriz.devcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
