9321883c40dbd4f05154e00659f22b396c0ff60d
giovanacgois <giovanagois@alunos.utfpr.edu.br>
2019-10-31 02:36:58 -0200

    Adicionado método salvarIsuario()

--- src/main/java/com/maptriz/devcase/controller/UsuarioResource.java
+++ src/main/java/com/maptriz/devcase/controller/UsuarioResource.java
@@ -9,8 +9,11 @@ import com.maptriz.devcase.model.Usuario;
 import com.maptriz.devcase.repository.UsuarioRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,13 +28,28 @@ public class UsuarioResource {
     @Autowired
     UsuarioRepository usuarioRepository;
     
+    /*
+    * GET 
+    */
+    
     @GetMapping("/usuarios")
-    public List<Usuario> listaUsuarios(){
+    public List<Usuario> listarUsuarios(){
     return usuarioRepository.findAll();
     }
     
     @GetMapping("/usuario/{id}")
-    public Usuario listaUsuarioUnico(@PathVariable(value="id")long id){
+    public Usuario listarUsuarioUnico(@PathVariable(value="id")long id){
     return usuarioRepository.findById(id);
     }
+
+    /*
+    * POST  
+    */
+    
+    @PostMapping("/usuario")
+    public Usuario salvarUsuario(@RequestBody Usuario usuario){
+    return usuarioRepository.save(usuario);
+    }
+
+
 }
