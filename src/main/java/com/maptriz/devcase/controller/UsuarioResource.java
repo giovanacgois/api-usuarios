06685e73b0d0c17d66a66f9e437d2bd19eba23b8
giovanacgois <giovanagois@alunos.utfpr.edu.br>
2019-10-31 02:12:47 -0200

    Adicionado método listaUsuarioUnico()

--- src/main/java/com/maptriz/devcase/controller/UsuarioResource.java
+++ src/main/java/com/maptriz/devcase/controller/UsuarioResource.java
@@ -10,6 +10,7 @@ import com.maptriz.devcase.repository.UsuarioRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,4 +30,8 @@ public class UsuarioResource {
     return usuarioRepository.findAll();
     }
     
+    @GetMapping("/usuario/{id}")
+    public Usuario listaUsuarioUnico(@PathVariable(value="id")long id){
+    return usuarioRepository.findById(id);
+    }
 }
