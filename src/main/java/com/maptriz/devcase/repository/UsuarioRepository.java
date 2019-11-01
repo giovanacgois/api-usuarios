ebfd39801319274e474b94e4562618fb4b0e72c2
giovanacgois <giovanagois@alunos.utfpr.edu.br>
2019-11-01 14:06:30 -0200

    Inclusão da assinatura do método findByDeletado

--- src/main/java/com/maptriz/devcase/repository/UsuarioRepository.java
+++ src/main/java/com/maptriz/devcase/repository/UsuarioRepository.java
@@ -6,14 +6,14 @@
 package com.maptriz.devcase.repository;
 
 import com.maptriz.devcase.model.Usuario;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  *
  * @author giovanacgois
  */
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>, UsuarioRepositoryCustom {
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    
-    Usuario findById(long id);
+    public List<Usuario> findByDeletado(boolean b);
 }
