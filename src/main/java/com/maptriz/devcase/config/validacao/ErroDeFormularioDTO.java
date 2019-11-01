5804d18ee37034611202bfa9c0aa3edc29cb62d0
giovanacgois <giovanagois@alunos.utfpr.edu.br>
2019-11-01 14:13:20 -0200

    Criação DTO para os Erros de Formulário

---
+++ src/main/java/com/maptriz/devcase/config/validacao/ErroDeFormularioDTO.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.maptriz.devcase.config.validacao;
+
+/**
+ *
+ * @author giovanacgois
+ */
+class ErroDeFormularioDTO {
+
+    private String campo;
+    private String erro;
+
+    public ErroDeFormularioDTO(String campo, String erro) {
+        this.campo = campo;
+        this.erro = erro;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
+
+    public String getErro() {
+        return erro;
+    }
+
+    public void setErro(String erro) {
+        this.erro = erro;
+    }
+
+}
